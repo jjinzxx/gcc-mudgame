2ca092d184c4c08517d5f691deae7a22e940def8
jjinzxx <epspqm823@gmail.com>
2026-04-25 12:20:31 +0900

    feat: Card.h/cpp 헤더정의 및 기능(함수)구현.

--- gcc-mudgame/Card.cpp
+++ gcc-mudgame/Card.cpp
@@ -1,1 +1,20 @@
 #include "Card.h"
+using namespace std;
+
+Card::Card(Suit suit, Rank rank) : suit(suit), rank(rank) {}
+
+int Card::getValue()
+{
+    int value = static_cast<int>(rank); // 1부터 킹까지 열거형 자료형을 int형 정수로 명시적 형변환
+    if (value > 10) return 10; // J, Q, K은 10점 처리
+    if (value == 1) return 11; // Ace는 일단 11점 처리
+    return value;
+}
+
+string Card::getName()
+{
+    string suitName[] = {"Spade", "Diamond", "Heart", "Club"};
+    string rankName[] = {"", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    
+    return suitName[static_cast<int>(suit)] + " " + rankName[static_cast<int>(rank)];
+}
--- gcc-mudgame/Card.h
+++ gcc-mudgame/Card.h
@@ -1,7 +1,18 @@
 #pragma once
+#include <string>
+using namespace std;
+
+enum class Suit {Spade,Diamond, Heart, Club};
+enum class Rank {Ace=1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King};
 
 class Card
 {
-public:
+private:
+    Suit suit;
+    Rank rank;
     
+public:
+    Card(Suit suit, Rank rank);
+    int getValue();  // 1부터 킹까지를 숫자로 표현해줄 함수
+    string getName(); // 카드모양과 숫자를 이름으로 반환해줄 함수
 };
